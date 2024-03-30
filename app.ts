f701d6ebafdce7f4f58b44e8302691af75832ddb
nardo88 <nardo1988@mail.ru>
2024-03-30 11:01:53 +0300

    1

--- app.ts
+++ app.ts
@@ -1,11 +1,9 @@
-// eslint-disable-next-line
 import express, { Request, Response } from 'express'
-
 const PORT = 4000
 const app = express()
 
 app.get('/', (req: Request, res: Response) => {
-  res.send("<h1>It's work</h1>")
+  res.send(`<h1>It's work</h1>`)
 })
 
 app.listen(PORT, () => {
