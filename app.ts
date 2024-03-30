1e038b30165f0620d9b71b869d752851148623a3
nardo88 <nardo1988@mail.ru>
2024-03-30 11:14:32 +0300

    add

--- app.ts
+++ app.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from 'express'
+import cors from 'cors'
+
 const PORT = 4000
 const app = express()
+
+app.use(cors())
 
 app.get('/', (req: Request, res: Response) => {
   res.send(`<h1>It's work</h1>`)
