52be3d1afc5b0ebd3fa0eed51ba4a21eb3011776
nardo88 <nardo1988@mail.ru>
2024-04-20 19:48:49 +0300

    1

--- app.ts
+++ app.ts
@@ -1,45 +1,24 @@
 import 'module-alias/register'
 import express, { Request, Response } from 'express'
-// import passport from 'passport'
-// import mongoose from 'mongoose'
-import cors from 'cors'
-// import createRoutes from './routes'
 
-// import dotenv from 'dotenv'
-// dotenv.config()
+import cors from 'cors'
 
 const PORT = 5000
-// const mongoUrl =
-//   process.env.MONGODB_URL || 'mongodb://localhost:27017/conspects'
 
 const app = express()
 
-// app.use(
-//   cors({
-//     origin: [PORT.toString()],
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-//     credentials: true,
-//   })
-// )
 app.use(cors())
 app.use(express.json())
-// app.use(passport.initialize())
-// const routers = createRoutes()
 
 app.get('/', (_req: Request, res: Response) => {
   res.json({ message: 'welcome to my app' })
 })
-// app.use('/api/v1', routers.authRouter)
-// app.use('/api/v1/users', routers.userRouter)
 
 app.use((_req: Request, res: Response) => {
   res.status(404).json({ message: 'Rout not found' })
 })
-// yo
 async function start() {
   try {
-    // await mongoose.connect(mongoUrl)
-
     app.listen(PORT, () => {
       console.log(`Server started on port ${PORT}`)
     })
