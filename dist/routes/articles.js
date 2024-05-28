dd0f483cf7300b3291d1de0ba2d1fb24fa58c0de
nardo88 <nardo1988@mail.ru>
2024-05-28 23:00:36 +0300

    fix

--- dist/routes/articles.js
+++ dist/routes/articles.js
@@ -3,10 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.createArticleRouter = void 0;
 const express_1 = require("express");
 const articles_1 = require("../controllers/articles");
+const auth_middleware_1 = require("../middleware/auth.middleware");
 const createArticleRouter = () => {
     const router = (0, express_1.Router)();
     const controller = new articles_1.ArticleController();
-    router.get('/', controller.getList);
+    router.get('/', auth_middleware_1.authStrict, controller.getList);
     router.get('/catalog', controller.getCatalog);
     return router;
 };
--- routes/articles.ts
+++ routes/articles.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import { AuthController } from '../controllers/auth'
 import { ArticleController } from '../controllers/articles'
+import { authStrict } from '../middleware/auth.middleware'
 
 export const createArticleRouter = (): Router => {
   const router = Router()
   const controller = new ArticleController()
 
-  router.get('/', controller.getList)
+  router.get('/', authStrict, controller.getList)
   router.get('/catalog', controller.getCatalog)
 
   return router
